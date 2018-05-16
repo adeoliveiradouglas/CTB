86274b1ad0ded546581c6bde14613964cfd0b76b
adeoliveiradouglas <adeoliveiradouglas@gmail.com>
2018-05-16 03:23:37 -0300

    Query sql para criar o banco

--- sql query.sql
+++ sql query.sql
@@ -1,24 +1,27 @@
-create table grupo(
+drop database gestaodecontratos;
+create database gestaodecontratos;
+
+create table gestaodecontratos.grupo(
 	idGrupo int primary key not null auto_increment,
     nome varchar(50) not null,
     descricao varchar(200) not null
 );
 
-create table permissao(
+create table gestaodecontratos.permissao(
 	idPermissao int primary key not null auto_increment,
     permissao varchar(50) not null,
     descricao varchar(100) not null
 );
 
-create table usuario(
+create table gestaodecontratos.usuario(
 	idUsuario int primary key not null auto_increment,
     nome varchar(60) not null,
     login varchar(60) not null,
     senha varchar(400) not null,
-    ativo int(1) not null
+    ativo boolean not null
 );
 
-CREATE TABLE usuarioXgrupo
+CREATE TABLE gestaodecontratos.usuarioXgrupo
 (
   idUsuario INT NOT NULL,  
   idGrupo   INT NOT NULL,
@@ -27,7 +30,7 @@ CREATE TABLE usuarioXgrupo
   FOREIGN KEY(idGrupo)  REFERENCES grupo(idGrupo)
 );
 
-CREATE TABLE permissaoXgrupo
+CREATE TABLE gestaodecontratos.permissaoXgrupo
 (
   idPermissao INT NOT NULL,  
   idGrupo     INT NOT NULL,
@@ -38,34 +41,33 @@ CREATE TABLE permissaoXgrupo
 
 
 /*Grupos*/
-insert into grupo (nome, descricao) values ('ADMINISTRADORES', 'Administrador');
-insert into grupo (nome, descricao) values ('USUARIOS', 'Usu·rios comuns');
-insert into grupo (nome, descricao) values ('BACKOFFICE', 'Cadastro');
+insert into gestaodecontratos.grupo (nome, descricao) values ('ADMINISTRADORES', 'Administrador');
+insert into gestaodecontratos.grupo (nome, descricao) values ('USUARIOS', 'Usu√°rios comuns');
+insert into gestaodecontratos.grupo (nome, descricao) values ('BACKOFFICE', 'Cadastro');
 
 /*Usuarios*/
-insert into usuario (nome, login, senha, ativo) values ('Douglas Almeida', 'douglas', '$2a$10$rQaIkdsWCvF2.LEhPm1UWu4Zy9tU3XhFqB4ascKWxRCgTGbjRBtV2' ,1);
+insert into gestaodecontratos.usuario (nome, login, senha, ativo) values ('Douglas Almeida', 'douglas', '$2a$10$rQaIkdsWCvF2.LEhPm1UWu4Zy9tU3XhFqB4ascKWxRCgTGbjRBtV2' , true);
 
-/*Permissıes*/
-insert into permissao (permissao, descricao) values ('ROLE_CADASTROUSUARIO', 'Cadastro de novos usu·rios');
-insert into permissao (permissao, descricao) values ('ROLE_CONSULTAUSUARIO', 'Consulta de usu·rios');
-insert into permissao (permissao, descricao) values ('ROLE_ADMIN', 'AdministraÁ„o');
+/*Permiss√µes*/
+insert into gestaodecontratos.permissao (permissao, descricao) values ('ROLE_CADASTROUSUARIO', 'Cadastro de novos usu√°rios');
+insert into gestaodecontratos.permissao (permissao, descricao) values ('ROLE_CONSULTAUSUARIO', 'Consulta de usu√°rios');
+insert into gestaodecontratos.permissao (permissao, descricao) values ('ROLE_ADMIN', 'Administra√ß√£o');
 
 /*Usuario em grupo*/
-set @idU := (select idUsuario from usuario where login = 'douglas');
-set @idG := (select idGrupo from grupo where nome = 'ADMINISTRADORES');
-
-insert into usuarioxgrupo (idUsuario, idGrupo) values (@idU, @idG);
+set @idU := (select idUsuario from gestaodecontratos.usuario where login = 'douglas');
+set @idG := (select idGrupo from gestaodecontratos.grupo where nome = 'ADMINISTRADORES');
+insert into gestaodecontratos.usuarioXgrupo (idUsuario, idGrupo) values (@idU, @idG);
 
 /*Permissoes para grupos*/
-set @idP := (select idPermissao from permissao where permissao = 'ROLE_ADMIN');
-set @idG := (select idGrupo from grupo where nome = 'ADMINISTRADORES');
-insert into permissaoxgrupo (idPermissao, idGrupo) values (@idP, @idG);
-set @idG := (select idGrupo from grupo where nome = 'USUARIOS');
-set @idP := (select idPermissao from permissao where permissao = 'ROLE_CONSULTAUSUARIO');
-insert into permissaoxgrupo (idPermissao, idGrupo) values (@idP, @idG);
-set @idG := (select idGrupo from grupo where nome = 'BACKOFFICE');
-set @idP := (select idPermissao from permissao where permissao = 'ROLE_CADASTROUSUARIO');
-insert into permissaoxgrupo (idPermissao, idGrupo) values (@idP, @idG);
+set @idP := (select idPermissao from gestaodecontratos.permissao where permissao = 'ROLE_ADMIN');
+set @idG := (select idGrupo from gestaodecontratos.grupo where nome = 'ADMINISTRADORES');
+insert into gestaodecontratos.permissaoXgrupo (idPermissao, idGrupo) values (@idP, @idG);
+set @idG := (select idGrupo from gestaodecontratos.grupo where nome = 'USUARIOS');
+set @idP := (select idPermissao from gestaodecontratos.permissao where permissao = 'ROLE_CONSULTAUSUARIO');
+insert into gestaodecontratos.permissaoXgrupo (idPermissao, idGrupo) values (@idP, @idG);
+set @idG := (select idGrupo from gestaodecontratos.grupo where nome = 'BACKOFFICE');
+set @idP := (select idPermissao from gestaodecontratos.permissao where permissao = 'ROLE_CADASTROUSUARIO');
+insert into gestaodecontratos.permissaoXgrupo (idPermissao, idGrupo) values (@idP, @idG);
 
 /*
 insert into  () values ();
