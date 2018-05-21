742a6e339060e243eb52e3aaf357d1f67c64b565
Douglas Almeida <adeoliveiradouglas@gmail.com>
2018-05-21 17:10:06 -0300

    Main

--- src/GestaoDeContratos.java
+++ src/GestaoDeContratos.java
@@ -1,13 +1,21 @@
-import dao.DAO;
+import java.util.ArrayList;
+
 import dao.UsuarioDAO;
 import entity.Usuario;
 
 public class GestaoDeContratos {
 
 	public static void main(String[] args) {
-		DAO dbUsuario = new UsuarioDAO("gestaodecontratos","root", "");
-		Usuario usuario = new Usuario("Extraterrestre", "et",
-				"$2a$10$C9eu2/MTCpgB6XuNXvMRi.0o45dSRKSViS3lFdks1gFidr9t2fzeK", true);
-		dbUsuario.inserir(usuario);
+//		Usuario usuario = new Usuario("Extraterrestre", "et", "$2a$10$C9eu2/MTCpgB6XuNXvMRi.0o45dSRKSViS3lFdks1gFidr9t2fzeK", true);
+		UsuarioDAO dbUsuario = new UsuarioDAO("gestaodecontratos","douglas", "administrador", "10.95.1.247");
+
+		
+//		dbUsuario.inserir(usuario);
+		
+		@SuppressWarnings("unused")
+		ArrayList<Usuario> usuarios = dbUsuario.getAll();
+		for(int i=0;i<1;++i){
+			
+		}
 	}
 }
