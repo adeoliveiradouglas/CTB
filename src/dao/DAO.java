1c239fa122e55dd70adfada1327393d9b82747f0
Douglas Almeida <adeoliveiradouglas@gmail.com>
2018-05-21 17:15:09 -0300

    Super classe do CRUD
    Atributos com anotacao @Data do framework Lombok para gerar getters e setters

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -4,111 +4,55 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import entity.Usuario;
 
 import DBConnection.DBConnection;
+import lombok.Data;
 
+@Data
 public class DAO {
 	private Connection dbConnection;
-	private String sqlQuery = null, nomeTabela = null, nomeBanco = null,
-			usuarioBanco = null, senhaBanco = null;
+	private String sqlQuery = null, 
+				   nomeTabela = null, 
+				   nomeBanco = null,
+				   usuarioBanco = null, 
+				   senhaBanco = null,
+				   ip = "localhost";
 	private ResultSet select = null;
-	private PreparedStatement stmt = null;
+	private PreparedStatement statement = null;
+	
+	public DAO(String nomeDB, String usuarioDB, String senhaDB, String tabelaBD, String ip) {
+		this.nomeBanco = nomeDB;
+		this.usuarioBanco = usuarioDB;
+		this.senhaBanco = senhaDB;
+		this.nomeTabela = tabelaBD;
+		this.ip = ip;
+	}
 
-	public DAO(String nomeDB, String usuarioDB, String senhaDB, String tabelaBD) {
+	/*public DAO(String nomeDB, String usuarioDB, String senhaDB, String tabelaBD) {
 		this.nomeBanco = nomeDB;
 		this.usuarioBanco = usuarioDB;
 		this.senhaBanco = senhaDB;
 		this.nomeTabela = tabelaBD;		
-	}
-
-	public Connection getDbConnection() {
-		return dbConnection;
-	}
-
-	public String getSqlQuery() {
-		return sqlQuery;
-	}
-
-	public ResultSet getSelect() {
-		return select;
-	}
-
-	public PreparedStatement getStatement() {
-		return stmt;
-	}
-
-	public String getNomeTabela() {
-		return nomeTabela;
-	}
-
-	public void setDbConnection(Connection dbConnection) {
-		this.dbConnection = dbConnection;
-	}
-
-	public void setSqlQuery(String sqlQuery) {
-		this.sqlQuery = sqlQuery;
-	}
-
-	public void setSelect(ResultSet select) {
-		this.select = select;
-	}
-
-	public void setStatement(PreparedStatement stmt) {
-		this.stmt = stmt;
-	}
-
-	public void setNomeTabela(String n) {
-		nomeTabela = n;
-	}
+	}*/
 
 	protected void iniciaConexaoComBanco() {
-		// inicia a conexï¿½o com o banco de dados
-		dbConnection = new DBConnection(nomeBanco, usuarioBanco, senhaBanco)
-		.getConnection();
+		// inicia a conexão com o banco de dados
+		this.dbConnection = new DBConnection(ip, nomeBanco, usuarioBanco, senhaBanco).getConnection();
 	}
 
 	protected void encerraConexaocomBanco() {
 		try {
-			dbConnection.close();
-			limpaVariaveis();
+			this.dbConnection.close();
+			this.limpaVariaveis();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
 	protected void limpaVariaveis() {
-		// reseta o conteï¿½do de todas as variï¿½veis desse objeto
+		// reseta o conteúdo de todas as variáveis desse objeto
 		this.select = null;
 		this.sqlQuery = null;
-		this.stmt = null;
+		this.statement = null;
 	}
-
-	// CRUD para usuario
-	public void inserir(Usuario u) {
-		// TODO Auto-generated method stub
-	}
-
-	public void atualizar(Usuario u) {
-
-	}
-
-	public Usuario getByName(Usuario u) {
-		return null;
-	}
-
-	public ArrayList<Usuario> getAll() {
-		return null;
-	}
-
-	public Usuario getByName(String nome) {
-		return null;
-	}
-
-	public void delete(Usuario usuario) {
-
-	}
-	
-	//CRUD para grupo
 }
