8a4b1326c06f7b6cd018dee7a23e82bea9499a6f
Douglas Almeida <adeoliveiradouglas@gmail.com>
2018-05-29 17:25:10 -0300

    DAO Usuario; Refazer as operações

--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -4,17 +4,17 @@
 
 package dao;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 import entity.Usuario;
 
 public class UsuarioDAO extends DAO {
-	String  nomeColunaId = "idUsuario", 
+	//Nome das colunas no banco de dados
+	String  nomeColunaMatricula = "matricula", 
 			nomeColunaNome = "nome", 
 			nomeColunaEmail = "email", 
 			nomeColunaSenha = "senha",
-			nomeColunaAtivo = "ativo";
+			nomeColunaSetor = "setor_codigo";
 
 	/*
 	 * public UsuarioDAO(String nomeDB, String usuarioDB, String senhaDB){
@@ -30,219 +30,22 @@ public class UsuarioDAO extends DAO {
 	}
 
 	public void inserir(Usuario usuario) {
-		// Insere usuario no banco
-		super.iniciaConexaoComBanco();
-		super.setSqlQuery("insert into " + super.getNomeTabela() + // nome da
-																	// tabela
-				" (" + nomeColunaNome + ", " + 
-					nomeColunaEmail + ", " + 
-					nomeColunaSenha + ", " + 
-					nomeColunaAtivo + ")" + // campos
-					"values (?,?,?,?)");
-
-		try {
-			super.setStatement(super.getDbConnection().prepareStatement(super.getSqlQuery()));
-			super.getStatement().setString(1, usuario.getNome());
-			super.getStatement().setString(2, usuario.getEmail());
-			super.getStatement().setString(3, usuario.getSenha());
-			super.getStatement().setBoolean(4, true);
-
-			super.getStatement().execute();
-			super.getStatement().close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			super.encerraConexaocomBanco();
-		}
+		
 	}
 
 	public void atualizar(Usuario usuario) {
-		super.iniciaConexaoComBanco();
-
-		super.setSqlQuery("update " + super.getNomeTabela() + " set " + nomeColunaNome + " = ?, " + nomeColunaEmail
-				+ " = ?, " + nomeColunaSenha + " = ?, " + nomeColunaAtivo + " = ?, " + // campos
-																						// para
-																						// inserir
-																						// na
-																						// tabela
-				"where " + nomeColunaId + " = ?");
-
-		try {
-			super.setStatement(super.getDbConnection().prepareStatement(super.getSqlQuery()));
-			super.getStatement().setString(1, usuario.getNome());
-			super.getStatement().setString(2, usuario.getEmail());
-			super.getStatement().setString(3, usuario.getSenha());
-			super.getStatement().setBoolean(4, usuario.isAtivo());
-			super.getStatement().setInt(5, usuario.getId());
-
-			super.getStatement().execute();
-			super.getStatement().close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			super.encerraConexaocomBanco();
-		}
-	}
-
-	public void delete(Usuario usuario) { // falta implementar
-		super.iniciaConexaoComBanco();
-		super.encerraConexaocomBanco();
-	}
-
-	public Usuario getByName(String nome) {
-		// select que retorna um unico usuario pesquisado pelo nome
-		super.iniciaConexaoComBanco();
-		// campos para pesquisar na tabela
-		super.setSqlQuery("select * from usuario where nome = '" + nome + "'"); //não funciona 
-//		super.setSqlQuery("select * from usuario where nome = ?");
 		
-		Usuario usuario = null;
-
-		try {
-			// Buscar usuario no banco
-			// prepara o Statement
-			super.setStatement(
-				super.getDbConnection().prepareStatement(
-					super.getSqlQuery()
-				)
-			);
-			
-			// completa as ? do Statement
-			super.getStatement().setString(1, super.getNomeTabela());
-			super.getStatement().setString(2, this.nomeColunaNome);
-			super.getStatement().setString(3, nome);
-			super.getStatement().setString(1, nome);
-			// executa a query e guarda na variavel super.select
-			super.setSelect(
-				super.getStatement().executeQuery()
-			);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			// Traduz o resultado para um objeto Usuario
-			usuario = new Usuario();
-			usuario.setId(super.getSelect().getInt(nomeColunaId));
-			usuario.setNome(super.getSelect().getString(nomeColunaNome));
-			usuario.setEmail(super.getSelect().getString(nomeColunaEmail));
-			usuario.setSenha(super.getSelect().getString(nomeColunaSenha));
-			usuario.setAtivo(super.getSelect().getBoolean(nomeColunaAtivo));
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			super.encerraConexaocomBanco();
-//			try {
-//				super.getStatement().close();
-//			} catch (SQLException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-		}
-
-		return usuario;
 	}
 
 	public Usuario getByEmail(String email) {
-		// select que retorna um unico usuario pesquisado pelo email
-		super.iniciaConexaoComBanco();
-		super.setSqlQuery("select * from " + super.getNomeTabela() + // nome da
-																		// tabela
-				" where " + nomeColunaEmail +" = ?"); // campos para pesquisar na tabela
-		Usuario usuario = null;
-
-		try {
-			// Buscar usuario no banco
-			// prepara o Statement
-			super.setStatement(super.getDbConnection().prepareStatement(super.getSqlQuery()));
-			// completa as ? do Statement
-			super.getStatement().setString(1, email);
-			// executa a query e guarda na variavel super.select
-			super.setSelect(super.getStatement().executeQuery());
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		// Traduz o resultado para um objeto Usuario
-		usuario = traduzirResultset();
-		/* forma antes da função traduzir result
-		 * usuario.setId(super.getSelect().getInt(nomeColunaId));
-		 * usuario.setNome(super.getSelect().getString(nomeColunaNome));
-		 * usuario.setEmail(super.getSelect().getString(nomeColunaEmail));
-		 * usuario.setSenha(super.getSelect().getString(nomeColunaSenha));
-		 * usuario.setAtivo(super.getSelect().getBoolean(nomeColunaAtivo));
-		 */
-		try {
-			super.getStatement().close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		super.encerraConexaocomBanco();
-		
-		return usuario;
+		return null;
 	}
 
 	public ArrayList<Usuario> getAll() {
-		// select que retorna todos os usuarios cadastrados no banco
-		super.iniciaConexaoComBanco();
-
-		setSqlQuery("select * from " + getNomeTabela());
-		ArrayList<Usuario> usuarios = new ArrayList<>(); // guarda os usuarios
-															// da query
-
-		try { // Buscar usuarios no banco
-			super.setStatement(super.getDbConnection().prepareStatement(super.getSqlQuery()));
-			super.setSelect(super.getStatement().executeQuery());
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-
-		try {
-			// Traduz o resultado para um objeto Usuario e insere na lista
-			while (getSelect().next()) {
-				Usuario usuario = new Usuario();
-				usuario.setId(getSelect().getInt("idUsuario"));
-				usuario.setNome(getSelect().getString("nome"));
-				usuario.setEmail(getSelect().getString("email"));
-				usuario.setSenha(getSelect().getString("senha"));
-				usuario.setAtivo(getSelect().getBoolean("ativo"));
-				// insere na lista
-				usuarios.add(usuario);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				super.getStatement().close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			encerraConexaocomBanco();
-		}
-
-		return usuarios;
+		return null;
 	}
 
-	private Usuario traduzirResultset() {
-		// Traduz o resultado de uma consulta para um objeto Usuario
-		Usuario usuario = new Usuario();
-		boolean erroResult = false; //controla se houve erro na busca do usuario
-		try {
-			usuario.setId(super.getSelect().getInt(this.nomeColunaId));
-			usuario.setNome(super.getSelect().getString(this.nomeColunaNome));
-			usuario.setEmail(super.getSelect().getString(this.nomeColunaEmail));
-			usuario.setSenha(super.getSelect().getString(this.nomeColunaSenha));
-			usuario.setAtivo(super.getSelect().getBoolean(this.nomeColunaAtivo));
-		} catch (SQLException e) {
-			erroResult = true;
-		}
-
-		if (erroResult){ 
-			//se houve erros no resultado retorna nada
-			return null;
-		} else {
-			return usuario;
-		}
-	}
+	/*private ArrayList<Usuario> traduzirResultset() {
+		return null;
+	}*/
 }
