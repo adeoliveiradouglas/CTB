7f42a3ad1b1422b36251a8bbd7e2e4080268f0fd
Douglas Almeida <adeoliveiradouglas@gmail.com>
2018-08-07 11:56:23 -0300

    Novas funções para envio padronizado

--- src/utilidades/Email.java
+++ src/utilidades/Email.java
@@ -1,12 +1,8 @@
 package utilidades;
 
-import java.util.ArrayList;
-
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
-
-import entity.Usuario;
 
 public class Email {
 	private SimpleEmail email;
@@ -14,13 +10,13 @@ public class Email {
 			mensagem = null,
 			emailTo = null,
 			nome = "Gestão de Contratos",
-			smtp = "smtp.gmail.com",
-			senha = "raspiberrypi",
-			emailFrom = "adeoliveiradouglas@gmail.com";
-	private int smtpPorta = 587;
-			/*smtp = "smtp.office365.com",
+			smtp = "smtp.office365.com",
 			senha = "$TEci.2018$",
-			emailFrom = "contratos.ctb@ctb.ba.gov.br";*/
+			emailFrom = "contratos.ctb@ctb.ba.gov.br";
+//			smtp = "smtp.gmail.com",
+//			senha = "raspiberrypi",
+//			emailFrom = "adeoliveiradouglas@gmail.com";
+	private int smtpPorta = 587;
 
 	public Email(){}
 	
@@ -42,37 +38,28 @@ public class Email {
 	}
 	
 	
-	public void aviso90dias(ArrayList<Usuario> destinos){
+	public void aviso90dias(String emailTo){
 //		gestor do contrato, fiscal e o gestor geral
 		this.assunto = "Faltam 90 dias para vencimento do contrato";
 		this.mensagem = "faltam 90 dias para vencimento do contrato";
-		
-		for (Usuario u: destinos){
-			this.emailTo = u.getEmail();
-			this.enviar();
-		}
+		this.emailTo = emailTo;
+		this.enviar();
 	}
 
-	public void aviso60dias(ArrayList<Usuario> destinos){
+	public void aviso60dias(String emailTo){
 //		para os acima e mais o diretor
 		this.assunto = "Faltam 60 dias para vencimento do contrato";
 		this.mensagem = "faltam 60 dias para vencimento do contrato";
-		
-		for (Usuario u: destinos){
-			this.emailTo = u.getEmail();
-			this.enviar();
-		}
+		this.emailTo = emailTo;
+		this.enviar();
 	}
 	
-	public void aviso45dias(ArrayList<Usuario> destinos){
+	public void aviso45dias(String emailTo){
 //		todos e mais o presidente	
 		this.assunto = "Faltam 45 dias para vencimento do contrato";
 		this.mensagem = "faltam 45 dias para vencimento do contrato";
-		
-		for (Usuario u: destinos){
-			this.emailTo = u.getEmail();
-			this.enviar();
-		}
+		this.emailTo = emailTo;
+		this.enviar();
 	}
 	
 	private void enviar(){
