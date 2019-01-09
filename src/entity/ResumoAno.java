9a9da8931aea026b870bac7efa3f28a9960d09b0
Douglas Almeida <adeoliveiradouglas@gmail.com>
2019-01-09 17:19:59 -0300

    09/01/2019 - Classes para representar resumo do contrato

---
+++ src/entity/ResumoAno.java
@@ -0,0 +1,33 @@
+/*
+ * Classe usada para representar o ano no resumo do contrato 
+ * É usada somente quando o usuário pede o resumo do contrato na tela de visualização da planilha
+ */
+package entity;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+public class ResumoAno {
+	int ano;
+	BigDecimal total = new BigDecimal("0"),
+			   aditivo = new BigDecimal("0");
+	@Setter
+	BigDecimal saldo = new BigDecimal("0");
+	
+	public ResumoAno(int ano) {
+		this.ano = ano;
+	}
+	
+	public void somaTotal(BigDecimal valor) {
+		total = total.add(valor);
+	}
+	
+	public void somaAditivo(BigDecimal valor) {
+		aditivo = aditivo.add(valor); 
+	}
+}
